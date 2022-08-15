eda5ae4e1988e4e23de9e287527248bc20b070ba
Tim Hotfilter <tim.hotfilter@kit.edu>
2022-08-15 18:09:48 +0200

    log min as well

--- simple_resnet/run_resnet.py
+++ simple_resnet/run_resnet.py
@@ -17,19 +17,23 @@ import pandas as pd
 
 dev_string = "cuda" if torch.cuda.is_available() else "cpu"
 device = torch.device(dev_string)
-amaxes = {}
+metrics = {}
 
 def conv2d_predicate(module):
     return isinstance(module, torch.nn.Conv2d)
 
-def get_amax_hook(module, inp, outp, amax_name):
+def get_amax_hook(module, inp, outp, module_name):
     cpu_dev = torch.device("cpu")
 
     amax = torch.amax(inp[0]).to(cpu_dev)
-    prev_amax = amaxes[amax_name]
+    amin = torch.amin(inp[0]).to(cpu_dev)
+    prev_amax = metrics[module_name]['max']
+    prev_amin = metrics[module_name]['min']
 
-    compare = torch.stack((amax, prev_amax))
-    amaxes[amax_name] = torch.amax(compare)
+    compare_max = torch.stack((amax, prev_amax))
+    compare_min = torch.stack((amin, prev_amin))
+    metrics[module_name]['max'] = torch.amax(compare_max)
+    metrics[module_name]['min'] = torch.amin(compare_min)
 
 
 m = models.resnet50(pretrained=True)
@@ -44,21 +48,25 @@ transforms = transforms.Compose([transforms.Resize(256),
                                transforms.ToTensor(),
                                normalize])
 
-dataset = datasets.ImageFolder('/data/oq4116/imagenet/val', transforms)
-# indices = random.sample(range(len(dataset)), 100)
-# dataset = Subset(dataset, indices=indices)
+# dataset = datasets.ImageFolder('/data/oq4116/imagenet/val', transforms)
+dataset = datasets.ImageFolder('/home/oq4116/temp/ILSVRC/Data/CLS-LOC/val', transforms)
+indices = random.sample(range(len(dataset)), 100)
+dataset = Subset(dataset, indices=indices)
 
 dataloader = DataLoader(dataset, batch_size=128, shuffle=True, pin_memory=True)
 
 # modify model
 for name, module in m.named_modules():
     if conv2d_predicate(module):
-        init_tensor = torch.tensor(0.0)
+        init_tensor_min = torch.tensor(float('inf'))
+        init_tensor_max = torch.tensor(float('-inf'))
         # init_tensor.to(device)
-        amaxes[name] = init_tensor
+        metrics[name] = {}
+        metrics[name]['max'] = init_tensor_max
+        metrics[name]['min'] = init_tensor_min
 
         module.register_forward_hook(
-            lambda module, inp, out, amax_name=name: get_amax_hook(module, inp, out, amax_name)
+            lambda module, inp, out, module_name=name: get_amax_hook(module, inp, out, module_name)
         )
 
 m.eval()
@@ -69,12 +77,13 @@ with torch.no_grad():
 
 
 pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(amaxes)
+pp.pprint(metrics)
 
-for k, v in amaxes.items():
-    amaxes[k] = v.item()
+for k, v in metrics.items():
+    for k2, v2 in v.items():
+        metrics[k][k2] = v2.item()
 
-df = pd.DataFrame(data=amaxes, index=[0])
+df = pd.DataFrame(data=metrics, index=[0])
 df = (df.T)
 df.to_excel('data.xlsx')
 
