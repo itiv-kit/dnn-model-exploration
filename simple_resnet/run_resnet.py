63fbc1ed9ef6b2a9deed4b6bb97efa2d399519a4
Tim TitanV <hotfilter@kit.edu>
2022-08-15 17:58:49 +0200

    gpu changes on titan V

--- simple_resnet/run_resnet.py
+++ simple_resnet/run_resnet.py
@@ -15,21 +15,25 @@ from tqdm import tqdm
 import pandas as pd 
 
 
+dev_string = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device(dev_string)
 amaxes = {}
 
 def conv2d_predicate(module):
     return isinstance(module, torch.nn.Conv2d)
 
 def get_amax_hook(module, inp, outp, amax_name):
-    amax = torch.amax(inp[0])
+    cpu_dev = torch.device("cpu")
+
+    amax = torch.amax(inp[0]).to(cpu_dev)
     prev_amax = amaxes[amax_name]
-    if isinstance(prev_amax, float):
-        prev_amax = torch.tensor(prev_amax)
+
     compare = torch.stack((amax, prev_amax))
     amaxes[amax_name] = torch.amax(compare)
 
 
 m = models.resnet50(pretrained=True)
+m.to(device)
 
 # prepare dataset
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -40,16 +44,18 @@ transforms = transforms.Compose([transforms.Resize(256),
                                transforms.ToTensor(),
                                normalize])
 
-dataset = datasets.ImageFolder('/home/oq4116/temp/ILSVRC/Data/CLS-LOC/val/', transforms)
+dataset = datasets.ImageFolder('/data/oq4116/imagenet/val', transforms)
 # indices = random.sample(range(len(dataset)), 100)
 # dataset = Subset(dataset, indices=indices)
 
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=128, shuffle=True, pin_memory=True)
 
 # modify model
 for name, module in m.named_modules():
     if conv2d_predicate(module):
-        amaxes[name] = 0.0
+        init_tensor = torch.tensor(0.0)
+        # init_tensor.to(device)
+        amaxes[name] = init_tensor
 
         module.register_forward_hook(
             lambda module, inp, out, amax_name=name: get_amax_hook(module, inp, out, amax_name)
@@ -58,9 +64,10 @@ for name, module in m.named_modules():
 m.eval()
 with torch.no_grad():
     for X, y_true in tqdm(dataloader):
+        X = X.to(device)
         y_prob = m(X)
 
-        
+
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(amaxes)
 
