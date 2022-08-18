967ef918c8ab6acea8b3a9b50575afd356b476ee
Tim Hotfilter <tim.hotfilter@kit.edu>
2022-08-18 16:55:21 +0200

    update on explore script

---
+++ simple_resnet/exploration/commons.py
@@ -0,0 +1,24 @@
+import torch
+from torchvision import transforms, datasets
+import random
+from torch.utils.data import DataLoader, Subset
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+
+                                std=[0.229, 0.224, 0.225])
+
+transforms = transforms.Compose([transforms.Resize(256),
+                            transforms.CenterCrop(224),
+                            transforms.ToTensor(),
+                            normalize])
+
+# dataset = datasets.ImageFolder('/home/oq4116/temp/ILSVRC/Data/CLS-LOC/val', transforms)
+dataset = datasets.ImageFolder('/data/oq4116/imagenet/val', transforms)
+indices = random.sample(range(len(dataset)), 100)
+dataset_100 = Subset(dataset, indices=indices)
+indices = random.sample(range(len(dataset)), 5000)
+dataset_5000 = Subset(dataset, indices=indices)
+
+
+dev_string = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device(dev_string)
--- simple_resnet/exploration/explore.py
+++ simple_resnet/exploration/explore.py
@@ -1,4 +1,5 @@
 import sys
+import pickle
 import torch
 import torch.utils.data
 from torch import nn
@@ -12,42 +13,137 @@ from pytorch_quantization.tensor_quant import QuantDescriptor
 from torchvision import models, transforms, datasets
 from torch.utils.data import DataLoader, Subset
 
+import numpy as np 
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.optimize import minimize
+from pymoo.operators.sampling.rnd import IntegerRandomSampling
+from pymoo.operators.repair.rounding import RoundingRepair
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.mutation.pm import PolynomialMutation
+from pymoo.termination import get_termination
+
 from torchinfo import summary
 
 from pytorch_quantization import quant_modules
 quant_modules.initialize()
 
+from commons import device, dataset, dataset_100, dataset_5000
 
 #load model:
-torch.load('resnet50-calib.pth')
+model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
+model.to(device)
+model.load_state_dict(torch.load('resnet50-calib.pth', map_location=device))
 
 
-dataloader = DataLoader(dataset_10000, batch_size=64, shuffle=True, pin_memory=True)
-correct_pred = 0
-model = model.to(device)
-with torch.no_grad():
-    for X, y_true in tqdm(dataloader):
-        X = X.to(device)
-        y_true = y_true.to(device)
-        y_prob = model(X)
-        _, predicted_labels = torch.max(y_prob, 1)
+class QuantizationModel(nn.Module):
+    def __init__(self, model, device) -> None:
+        super().__init__()
 
-        correct_pred += (predicted_labels == y_true).sum()
+        self.model = model
+        self.bit_widths = {}
+        self.device = device
 
-accuracy = correct_pred.float() / len(dataloader.dataset)
-print(accuracy)
+    def set_bit_widths(self, bit_widths):
+        self.bit_widths = bit_widths
 
-quant_modules.deactivate()
-correct_pred = 0
-model = model.to(device)
-with torch.no_grad():
-    for X, y_true in tqdm(dataloader):
-        X = X.to(device)
-        y_true = y_true.to(device)
-        y_prob = model(X)
-        _, predicted_labels = torch.max(y_prob, 1)
+    def prepare_model(self):
+        for name, module in self.model.named_modules():
+            if isinstance(module, quant_nn.TensorQuantizer):
+                assert name in self.bit_widths, "Layer {} not found in bit_widths dict".format(name)
+                module.num_bits = self.bit_widths[name]
 
-        correct_pred += (predicted_labels == y_true).sum()
+    def evaluate(self, dataloader):
+        correct_pred = 0
+        self.model.eval()
+        self.model = self.model.to(self.device)
+        with torch.no_grad():
+            for X, y_true in tqdm(dataloader):
+                X = X.to(self.device)
+                y_true = y_true.to(self.device)
 
-accuracy = correct_pred.float() / len(dataloader.dataset)
-print(accuracy)
+                y_prob = self.model(X)
+                _, predicted_labels = torch.max(y_prob, 1)
+
+                correct_pred += (predicted_labels == y_true).sum()
+
+        accuracy = correct_pred.float() / len(dataloader.dataset)
+        return accuracy
+
+
+class LayerwiseQuantizationProblem(ElementwiseProblem):
+    def __init__(self, q_model, dataloader, n_var, layernames, **kwargs):
+        super().__init__(
+            n_var=n_var,
+            n_obj=2,
+            n_constr=1,
+            xl=2,
+            xu=16,
+            vtype=int,
+            **kwargs)
+        
+        self.q_model = q_model
+        self.dataloader = dataloader
+        self.layernames = layernames
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        bit_widths = {}
+        for i, name in enumerate(self.layernames):
+            bit_widths[name] = int(x[i])
+        print(x)
+        self.q_model.set_bit_widths(bit_widths)
+
+        self.q_model.prepare_model()
+        f1_acc = self.q_model.evaluate(self.dataloader)
+        f2_bits = np.sum(x)
+        print("acc of pass {}%".format(f1_acc * 100))
+        g1_acc_constraint = 0.70 - f1_acc
+        out["F"] = [-f1_acc, f2_bits]
+        out["G"] = [g1_acc_constraint]
+
+
+# get layernames
+layernames = []
+for name, module in model.named_modules():
+    if isinstance(module, quant_nn.TensorQuantizer):
+        layernames.append(name)
+
+print(layernames)
+# explore
+qmodel = QuantizationModel(model, device)
+
+dataloader = DataLoader(dataset_5000, batch_size=64, shuffle=True, pin_memory=True)
+problem = LayerwiseQuantizationProblem(
+    q_model=qmodel,
+    dataloader=dataloader,
+    n_var=len(layernames),
+    layernames=layernames
+)
+
+
+sampling = IntegerRandomSampling()
+crossover = SBX(prob_var=1.0, repair=RoundingRepair(), vtype=float)
+mutation = PolynomialMutation(prob=1.0, repair=RoundingRepair())
+
+algorithm = NSGA2(
+    pop_size=20,
+    n_offsprings=20,
+    sampling=sampling,
+    crossover=crossover,
+    mutation=mutation,
+    eliminate_duplicates=True)
+
+termination = get_termination("n_gen", 30)
+
+res = minimize(
+    problem,
+    algorithm,
+    termination,
+    verbose=True
+)
+
+with open('exploration.pkl', 'wb') as f:
+    pickle.dump(res, f)
+
+
+
