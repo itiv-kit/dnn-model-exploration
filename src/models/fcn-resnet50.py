183a821b4492527042a4125a0d9f09c1b3fe7d23
Tim Hotfilter <tim.hotfilter@kit.edu>
2022-11-17 17:57:24 +0100

    clean uop

--- src/models/fcn-resnet50.py
+++
@@ -1,3 +0,0 @@
-from torchvision import models
-
-model = models.segmentation.fcn_resnet50(pretrained=True)
--- src/models/fcos_resnet50_fpn.py
+++
@@ -1,3 +0,0 @@
-import torchvision.models as models
-
-model = models.detection.fcos_resnet50_fpn(pretrained = True)
